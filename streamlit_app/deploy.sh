d670919dcd46610547b4319e9f24fdf1bc17d792
Mohannad Jaradat <jaradatm2@hotmail.com>
2025-06-26 14:46:22 +0300

    deploy step

--- streamlit_app/deploy.sh
+++ streamlit_app/deploy.sh
@@ -4,27 +4,23 @@ set -e  # Exit on error
 set -o pipefail
 
 APP_DIR="/var/lib/jenkins/simple-jenkins-dockerized/streamlit_app"
+GIT_DIR="/var/lib/jenkins/simple-jenkins-dockerized"
 BRANCH="${1:-main}"
 
 echo "🚀 Deploying branch: $BRANCH"
 
-cd "$APP_DIR"
+cd "$GIT_DIR"
 
-# echo "🔄 Pulling latest changes..."
-# git fetch origin
-# git checkout "$BRANCH"
-# git reset --hard "origin/$BRANCH"
+echo "🔄 Pulling latest changes..."
+git fetch origin
+git checkout "$BRANCH"
+git reset --hard "origin/$BRANCH"
 
 # Check if port 8501 is in use and kill the process using it
 echo "🛑 Checking if port 8501 is in use..."
 
-PORT_IN_USE=$(lsof -ti:8501)
-if [ -n "$PORT_IN_USE" ]; then
-  echo "⚠️ Port 8501 is in use by PID(s): $PORT_IN_USE. Terminating them..."
-  kill -9 $PORT_IN_USE || true
-else
-  echo "✅ Port 8501 is free."
-fi
+pkill streamlit || true
+fuser -k 8501/tcp || true
 
 echo "🐳 Building & Starting Docker container..."
 docker-compose down
